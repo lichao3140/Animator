e3cfe063524f937ecc95792bba4f2c540fac55e9
lichao3140 <396229938@qq.com>
2017-10-21 19:28:47 +0800

    仿淘宝添加购物车动画

--- app/src/main/java/com/lqh/lichao/animator/TaobaoActivity.java
+++ app/src/main/java/com/lqh/lichao/animator/TaobaoActivity.java
@@ -66,4 +66,41 @@ public class TaobaoActivity extends AppCompatActivity {
         set.playTogether(firstScaleXAnim,firstScaleYAnim,firstAlphaAnim,firstRotationXAnim,firstResumeRotationXAnim,firstTranslationYAnim,secondTranslationYAnim);
         set.start();
     }
+
+    public void startSecondAnim(View v) {
+        //显示first_view:1.透明；2.缩放；3.翻转动画（以什么地方为原点进行翻转？）；4.
+        //缩放
+        ObjectAnimator firstScaleXAnim = ObjectAnimator.ofFloat(first_view, "scaleX", 0.8f, 1.0f);
+        firstScaleXAnim.setDuration(300);
+        ObjectAnimator firstScaleYAnim = ObjectAnimator.ofFloat(first_view, "scaleY",0.8f, 1.0f);
+        firstScaleYAnim.setDuration(300);
+        //透明度
+        ObjectAnimator firstAlphaAnim = ObjectAnimator.ofFloat(first_view, "alpha",0.5f, 1.0f);
+        firstAlphaAnim.setDuration(300);
+        //旋转
+        ObjectAnimator firstRotationXAnim = ObjectAnimator.ofFloat(first_view, "rotationX", 0f,20f);
+        firstRotationXAnim.setDuration(200);
+        //再旋转回来
+        ObjectAnimator firstResumeRotationXAnim = ObjectAnimator.ofFloat(first_view, "rotationX",20f, 0f);
+        firstResumeRotationXAnim.setDuration(200);
+        firstResumeRotationXAnim.setStartDelay(200);//延迟第一次旋转动画的时间，在这之后执行
+        //由于缩放造成离顶部有个距离，需要平移上去
+        ObjectAnimator firstTranlationYAnim = ObjectAnimator.ofFloat(first_view, "translationY",-0.1f*first_view.getHeight(), 0f);
+        firstTranlationYAnim.setDuration(300);
+        //第二个view和第一个view动画同时开始执行
+        ObjectAnimator secondTranlationYAnim = ObjectAnimator.ofFloat(second_view, "translationY",0f, second_view.getHeight());
+        secondTranlationYAnim.setDuration(300);
+        secondTranlationYAnim.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                // TODO Auto-generated method stub
+                super.onAnimationEnd(animation);
+                second_view.setVisibility(View.INVISIBLE);
+                bt.setClickable(true);
+            }
+        });
+        AnimatorSet set = new AnimatorSet();
+        set.playTogether(firstScaleXAnim,firstScaleYAnim,firstAlphaAnim,firstRotationXAnim,firstResumeRotationXAnim,firstTranlationYAnim,secondTranlationYAnim);
+        set.start();
+    }
 }
